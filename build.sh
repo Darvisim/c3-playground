0781f16f9878e901d24bcfd5bc13a15e25be2f33
Darvis <dsouzavishal685@gmail.com>
2026-09-25 01:28:24 +0530

    Use GROWABLE_ARRAYBUFFERS instead of ALLOW_MEMORY_GROWTH

--- build.sh
+++ build.sh
@@ -46,7 +46,7 @@ emar rcs "${SYS_LIB_DIR}/libm.a" "${SYS_LIB_DIR}/dummy_m.o"
 rm -f "${SYS_LIB_DIR}/dummy_m.c" "${SYS_LIB_DIR}/dummy_m.o"
 
 EMSCRIPTEN_COMPILE_ARGS="-matomics -mbulk-memory"
-EMSCRIPTEN_LINK_ARGS="-sALLOW_MEMORY_GROWTH=1 -sFORCE_FILESYSTEM=1 -sEXIT_RUNTIME=0 -sINITIAL_MEMORY=256MB -sSTACK_SIZE=8MB -sERROR_ON_UNDEFINED_SYMBOLS=0 -sEXPORTED_RUNTIME_METHODS=FS,callMain -sEXPORTED_FUNCTIONS=_main,_fflush --preload-file ${HOST_LIB_DIR}@/usr/lib/c3 --preload-file ${SYS_LIB_DIR}@/usr/lib/c3/wasm32-emscripten --preload-file ${RAYLIB_LIB}@/usr/lib/c3/lib/raylib6.c3l"
+EMSCRIPTEN_LINK_ARGS="-sGROWABLE_ARRAYBUFFERS=2 -sFORCE_FILESYSTEM=1 -sEXIT_RUNTIME=0 -sINITIAL_MEMORY=256MB -sSTACK_SIZE=8MB -sERROR_ON_UNDEFINED_SYMBOLS=0 -sEXPORTED_RUNTIME_METHODS=FS,callMain -sEXPORTED_FUNCTIONS=_main,_fflush --preload-file ${HOST_LIB_DIR}@/usr/lib/c3 --preload-file ${SYS_LIB_DIR}@/usr/lib/c3/wasm32-emscripten --preload-file ${RAYLIB_LIB}@/usr/lib/c3/lib/raylib6.c3l"
 
 # 2. Configure and compile c3c to WebAssembly
 meson setup ${BUILD_DIR} ${PROJECT_ROOT} \
